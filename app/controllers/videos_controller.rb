156cf70d23b79f898edf3e7db41e7f4450546aa0
SCA2 <tpryan_1965@yahoo.com>
2015-05-25 14:01:02 -0700

    remove test_environment helper

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,9 +1,5 @@
 class VideosController < ApplicationController
 
-  include ApplicationHelper
-
-  before_action :test_environment
-  
   def index
     @categories = Category.joins(:videos).order(:name).distinct
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,2 @@
 module ApplicationHelper
-
-  def test_environment
-    redirect_to :root if Rails.env.production?
-  end
-
 end
