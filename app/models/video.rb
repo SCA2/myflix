049894948da7a773fcef893959d8eb35565e46c0
SCA2 <tpryan_1965@yahoo.com>
2015-05-27 10:22:18 -0700

    add search_by_title and tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,4 +2,8 @@ class Video < ActiveRecord::Base
   belongs_to :category
   scope :sorted, -> { order(:title) }
   validates :title, :description, presence: true
+
+  def self.search_by_title(title)
+    self.where("title ILIKE ?", "%#{title}%").all.sorted
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -13,4 +13,33 @@ describe Video do
     expect(Video.all.sorted).to eq [a, b, c]
   end
 
+  describe "search_by_title" do
+    it "returns empty array" do
+      expect(Video.search_by_title("non-existent title")).to eq []
+    end
+    
+    it "returns one Video" do
+      video = Video.create(title: 'Title', description: 'Description')
+      expect(Video.search_by_title("Title")).to eq [video]
+    end
+
+    it "ignores case" do
+      video = Video.create(title: 'Title', description: 'Description')
+      expect(Video.search_by_title("title")).to eq [video]
+    end
+
+    it "returns array of Videos" do
+      a = Video.create(title: 'Title a', description: 'Description a')
+      b = Video.create(title: 'Title b', description: 'Description b')
+      c = Video.create(title: 'Title c', description: 'Description c')
+      expect(Video.search_by_title('title')).to eq [a, b, c]
+    end
+
+    it "returns array of Videos in order" do
+      c = Video.create(title: 'Title c', description: 'Description c')
+      b = Video.create(title: 'Title b', description: 'Description b')
+      a = Video.create(title: 'Title a', description: 'Description a')
+      expect(Video.search_by_title('title')).to eq [a, b, c]
+    end
+  end
 end
