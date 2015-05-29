dc7a27010f2e6d43fd2596e9a21a94685a7ce1b9
SCA2 <tpryan_1965@yahoo.com>
2015-05-29 10:40:10 -0700

    add user and session models, controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Myflix::Application.routes.draw do
-  get '/home', to: 'videos#index'
+  root                to: "pages#front"
+  get '/home',        to: 'videos#index'
+  get '/sign_in',     to: 'sessions#new'
+  post '/sign_in',    to: 'sessions#create'
   resources :videos, only: [:index, :show] do
     collection do
-      get :search, to: "videos#search"
+      get :search,    to: "videos#search"
     end
   end
   resources :categories, only: :show
+  resources :users, only: [:new, :create] do
+    collection do
+      get :front,     to: "users#front"
+    end
+  end
   get 'ui(/:action)', controller: 'ui'
 end
