8e59d2574b59b4183b6f43361762637eae906a45
SCA2 <tpryan_1965@yahoo.com>
2015-07-18 15:25:52 -0700

    add default host to test.rb

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,14 +1,14 @@
 class UserMailer < ActionMailer::Base
-  default from: "signup@myflix.com"
-  
+  default 
+
   def welcome(user)
     @user = user
-    mail(to: @user.email, subject: 'Welcome to MyFlix!')
+    mail from: 'signup@myflix.com', to: @user.email, subject: 'Welcome to MyFlix!'
   end
 
   def password_reset(user)
     @user = user
-    mail to: @user.email, subject: 'MyFlix Password Reset'
+    mail from: 'admin@myflix.com', to: @user.email, subject: 'MyFlix Password Reset'
   end
 
 end
