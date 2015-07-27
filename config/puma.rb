090e6c02f04b584bb1d58209d999a91a6c3634cf
SCA2 <tpryan_1965@yahoo.com>
2015-07-26 18:05:42 -0700

    edit puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -12,7 +12,9 @@ on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
-
+  
+  @sidekiq_pid ||= spawn("bundle exec sidekiq -c 2")
+  
   Sidekiq.configure_client do |config|
     config.redis = { size: 1, url: ENV["REDISTOGO_URL"] }
   end
