a63e9b03928d2f9f431ab14c6214fcbd48d0b62e
SCA2 <tpryan_1965@yahoo.com>
2015-08-11 22:12:40 -0700

    rename heroku app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,14 +18,14 @@ Myflix::Application.configure do
 
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { host: "floating-tor-1033.herokuapp.com" }
+  config.action_mailer.default_url_options = { host: "stormy-brook-2436.herokuapp.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port:                 ENV['MAILGUN_SMTP_PORT'],
     address:              ENV['MAILGUN_SMTP_SERVER'],
     user_name:            ENV['MAILGUN_SMTP_LOGIN'],
     password:             ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:               'floating-tor-1033.herokuapp.com',
+    domain:               'stormy-brook-2436.herokuapp.com',
     authentication:       :plain,
   }
 end
